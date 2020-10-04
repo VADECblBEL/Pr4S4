6ef2d75b0a50c3d6c7ae32c83e80cbfe74de3ec4
Вадим <vadimmm.m@ya.ru>
2020-10-04 12:41:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace Pr4S4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListDictionary ld = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+            ld["Estados Unidos"] = "USA";
+            ld["Canada"] = "Canada";
+            ld["Espana"] = "Spain";
+
+            Console.WriteLine(ld["Canada"]);
+            Console.WriteLine(ld["Espana"]);
+
+        }
+    }
+}
